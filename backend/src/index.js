7043de3ae2482a86b8be56836bfbc6f25a9a3fc8
umair <uamjad5009@gmail.com>
2026-01-26 13:00:44 +0500

    testing deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -100,7 +100,7 @@ app.use((err, req, res, next) => {
 setupWebSocket(io);
 
 // Connect to MongoDB and start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
